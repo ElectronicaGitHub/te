303b647b73dd06ea79703747c848cfdf760d2e91
Philip <molo4nik11@gmail.com>
2015-04-02 11:38:35 +0300

    сделан почти полностью рест

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -4,9 +4,31 @@ app.controller('Admin', ['$scope', '$http', function($scope, $http) {
 	$scope.themes = window.themes;
 	$scope.tours = window.tours;
 
+	$scope.loadDataToUpdate = function(data, type) {
+
+		if (type == 'tour') {
+			$scope.newtour = data;
+			$scope.updateButtonTour = true;
+		} else if (type == 'theme') {
+			$scope.newtheme = data;
+			$scope.updateButtonTheme = true;
+		}
+	}
+
 	$scope.add = function (type, obj) {
 		console.log('add', type , obj);
 		$http.post('/admin/save/' + type, obj)
+			.success(function(data) {
+				console.log(data);
+			})
+			.error(function(data) {
+				console.log(data);
+			})
+	}
+
+	$scope.update = function (type, obj) {
+		console.log('update', type , obj);
+		$http.post('/admin/update/' + type, obj)
 			.success(function(data) {
 				console.log(data);
 			})
--- routes/admin.js
+++ routes/admin.js
@@ -4,10 +4,10 @@ var Tour = require('../models/Tour');
 function admin(express) {
 	var router = express.Router();
 
-	router.get('/', function(req, res, next) {
-		Themes.find(function(err, json) {
+	router.get('/', function (req, res, next) {
+		Themes.find(function (err, json) {
 			if (err) return next(err);
-			Tour.find(function(err, json2) {
+			Tour.find(function (err, json2) {
 				if (err) return next(err);
 				res.render('admin', {
 					themes : json,
@@ -17,22 +17,53 @@ function admin(express) {
 		})
 	});
 
-	router.post('/save/theme', function(req, res, next) {
+	// saves
+
+	router.post('/save/theme', function (req, res, next) {
 		var t = new Themes(req.body);
-		t.save(function(err, results) {
+		t.save(function (err, results) {
 			if (err) return next(err);
 			res.json({
-				saved : 'ok'
+				result : 'theme saved'
 			});
 		})
 	})
 
-	router.post('/save/tour', function(req, res, next) {
+	router.post('/save/tour', function (req, res, next) {
 		var t = new Tour(req.body);
-		t.save(function(err, results) {
+		t.save(function (err, results) {
 			if (err) return next(err);
 			res.json({
-				saved : 'ok'
+				result : 'tour saved'
+			});
+		})
+	})
+
+	// updates
+
+	router.post('/update/theme', function (req, res, next) {
+		var t = req.body;
+		var id = req.body._id;
+		delete t._id;
+		delete t.__v;
+		delete t.tours;
+		Themes.findOneAndUpdate({ _id : id }, t, function (err, results) {
+			if (err) return next(err);
+			res.json({
+				result : 'theme updated'
+			});
+		})
+	})
+
+	router.post('/update/tour', function (req, res, next) {
+		var t = req.body;
+		var id = req.body._id;
+		delete t._id;
+		delete t.__v;
+		Tour.findOneAndUpdate({ _id : id }, t, function (err, results) {
+			if (err) return next(err);
+			res.json({
+				result : 'tour updated'
 			});
 		})
 	})
